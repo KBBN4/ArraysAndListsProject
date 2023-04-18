538037687d64ee8abcf02de86535f7e394f461fc
katherine Buendia <kbbuendia24@gmail.com>
2023-04-18 10:28:57 -0400

    Arrays Excercise

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 
 namespace ArraysAndLists
 {
@@ -9,24 +12,63 @@ namespace ArraysAndLists
             //TODO:
 
             // Create an int Array and populate numbers 1-10
+            int[] myArray = new int[] { 1,2,3,4,5,6,7,8,9,10 };
 
-            /* Create two Lists of type int.
-             * Name one List "evens"
-             * Name the other List "odds"
-             */
+            //Create two Lists of type int.
+            //* Name one List "evens"
+            //* Name the other List "odds"
 
-            /* Using either a foreach or for loop,
-             * nest an if statement to check to see
-             *  if a number is even or odd.
-             * Then add those numbers to either the evens List
-             * or the odds List
-             */
+            List<int> evens = new List<int>();
+            List<int> odds = new List<int>();
 
-            /* Now using foreach or for loops,
-             * display each List of even and odd numbers
-             *
-             * Try to be creative in your display
-             */
+     //       *Using either a foreach or for loop,
+     //*nest an if statement to check to see
+     //*  if a number is even or odd.
+     //* Then add those numbers to either the evens List
+     //*or the odds List
+     //*/
+
+    /* Now using foreach or for loops,
+     * display each List of even and odd numbers
+     *
+     * Try to be creative in your display
+     */
+
+
+
+
+            foreach (var num in myArray)
+            {
+                if ( num %2 == 0)
+                {
+                    evens.Add(num);
+                }
+                else
+                {
+                    odds.Add(num);
+                    
+                }
+            }
+
+            foreach (var num in evens)
+            {
+                Console.WriteLine($"your {num} is even");
+            }
+            foreach (var num in odds)
+            {
+                Console.WriteLine($" your {num} is odd");
+            }
+            
+                
+            
+            {
+
+            }
+        }   
+            
         }
+
     }
-}
+       
+    
+
